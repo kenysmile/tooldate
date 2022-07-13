46f88bf22f599fbcb411a8be1a340ad1b99707ea
thangld2 <thangld2@rikkeisoft.com>
2022-07-13 09:07:00 +0700

    template

--- tool/views.py
+++ tool/views.py
@@ -24,27 +24,29 @@ def create_tool_date(request):
         form = TooldateForm(request.POST)
         lst_extra_hours = request.POST['lst_extra_hours']
         start_date = request.POST['startdate']
+        set_hours_work = request.POST['sethourswork']
+        
         tool_date = ToolDate.objects.create(lst_extra_hours=lst_extra_hours, start_date=start_date)
         convert_start_date = datetime.strptime(tool_date.start_date, '%Y-%m-%d')
         start_date = convert_start_date
         time_out = 0
         for extra_hours in str(tool_date.lst_extra_hours).split('-'):
             extra_hours = str(extra_hours).replace(",",".")
-            if float(extra_hours) < 8:
+            if float(extra_hours) < float(set_hours_work):
                 end_date = start_date
                 time_out_choice = float(extra_hours) + time_out
-                if time_out_choice >= 8:
+                if time_out_choice >= float(set_hours_work):
                     days = float(time_out_choice)//8
-                    time_out_choice = (float(time_out_choice) % 8)
+                    time_out_choice = (float(time_out_choice) % float(set_hours_work))
                     end_date = start_date  + timedelta(days=days)
                 else:
                     time_out_choice = 0
             else:
                 days = float(extra_hours)//8
-                time_out_choice = (float(extra_hours) % 8) + time_out
+                time_out_choice = (float(extra_hours) % float(set_hours_work)) + time_out
                 end_date = start_date  + timedelta(days=days)
             weekday = start_date.weekday()
-            if weekday in [5, 6]:
+            if weekday == 4:
 
                 end_date += timedelta(days=2)
             tool_date_details = ToolDateDetails.objects.create(tool_date=tool_date, name=tool_date.pk, 
